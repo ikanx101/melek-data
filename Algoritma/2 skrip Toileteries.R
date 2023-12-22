6e9557254b115abf56df2894f9d7c7645a03c2a5
ikanx101 <ikanx101@gmail.com>
2023-12-22 09:04:01 +0000

    Toileteries update

--- Algoritma/2 skrip Toileteries.R
+++ Algoritma/2 skrip Toileteries.R
@@ -32,39 +32,11 @@ df_item_keluar = read_excel("DAFTAR ITEM TOILETRIS.xlsx",
                             sheet = toilet_sht[3]) %>% janitor::clean_names()
 # ==============================================================================
 
-
 # ==============================================================================
 # kita akan rapikan terlebih dahulu datanya
 df_item_keluar = 
   df_item_keluar %>% 
-  filter(jml_keluar > 0)
-
-# kita simpan nama pemohon untuk kita ganti alias
-user_pemohon_dbase = df_item_keluar$diterima_oleh %>% unique()
-nama_alias         = 
-  randomNames::randomNames(length(user_pemohon_dbase),which.names = "both",) %>% 
-  unique()
-
-# ini adalah referensi nama pemohon
-referensi_nama = data.frame(user_pemohon = user_pemohon_dbase,
-                            pemohon_nama = nama_alias)
-
-# kita simpan nama departemen untuk kita ganti alias
-dept_pemohon_dbase = df_item_keluar$dept_pemohon_bk %>% unique()
-dept_pemohon_alias = paste0(substr(dept_pemohon_dbase,2,3),
-                            substr(dept_pemohon_dbase,1,1)) |> toupper()
-
-# ini adalah referensi departemen
-referensi_dept = data.frame(dept_pemohon_bk = dept_pemohon_dbase,
-                            pemohon_dept    = dept_pemohon_alias)
-
-# kita gantikembali namanya
-df_item_keluar = 
-  df_item_keluar %>% 
-  merge(referensi_nama,by = "user_pemohon") %>% 
-  select(-user_pemohon) %>% 
-  merge(referensi_dept,by = "dept_pemohon_bk") %>% 
-  select(-dept_pemohon_bk,-diterima_oleh)
+  filter(!is.na(jml_keluar))
 # ==============================================================================
 
 
@@ -72,25 +44,28 @@ df_item_keluar =
 # kita save terlebih dahulu data itemnya terlebih dahulu
 dbase_produk = 
   df_item_masuk %>% 
-  select(kode_item,nama_item,satuan,penjual,harga_beli) %>% 
+  select(kode_item,nama_item,satuan,harga_beli) %>% 
   distinct() %>% 
+  mutate(harga_beli = as.numeric(harga_beli)) |>
+  group_by(kode_item,nama_item,satuan) |>
+  summarise(harga_beli = mean(harga_beli,na.rm = T)) |>
+  ungroup() |>
   arrange(kode_item) 
 
 # kita ambil data item masuk
 df_item_masuk = 
   df_item_masuk %>% 
   select(-input_oleh,-no_transaksi,-nama_item,
-         -harga_beli,-total_beli,-bulan,-tahun) %>% 
-  rename(dibeli_dari = penjual)
+         -harga_beli,-total_beli,-penjual,-periode,-nomor_po)
 
 # kita ambil data item keluar
 df_item_keluar = 
   df_item_keluar |>
-  select(-input_oleh,-nama_item,-bulan,-tahun,
-         -bukti_serah_terima,-no_transaksi)
+  select(-input_oleh,-nama_item,-keterangan,
+         -no_transaksi,-periode)
 
 # kita akan export kembali
-setwd("~/melek-data/Clean")
+setwd("~/melek-data/Clean/Toileteries")
 
 openxlsx::write.xlsx(dbase_produk,
                      file = "dbase produk.xlsx")
