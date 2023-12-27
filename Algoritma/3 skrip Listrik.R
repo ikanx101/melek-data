1ae0cf52daadfaead7501cede4e605e6a03b4375
ikanx101 <ikanx101@gmail.com>
2023-12-27 09:21:30 +0700

    update seluruh folder dan readme

--- Algoritma/3 skrip Listrik.R
+++ Algoritma/3 skrip Listrik.R
@@ -3,7 +3,7 @@
 # sebuah usaha membentuk insani berwawasan data dan berpikir kritis
 # 
 # skrip pembersih data
-# data toileteries
+# data listrik
 # dibuat oleh:
 # ikanx101.com
 # ==============================================================================
@@ -17,26 +17,30 @@ library(tidyr)
 library(readxl)
 
 # kita alihkan path nya ke folder raw data
-setwd("~/melek-data/Raw/Toileteries")
+setwd("~/melek-data/Raw/Listrik")
 
 # kita ambil semua sheet dalam file excelnya
-toilet_sht = excel_sheets("DAFTAR ITEM TOILETRIS.xlsx")
+atk_sht = excel_sheets("DAFTAR ITEM TEKNISI.xlsx")
 
-# kita akan ambil tiga data terlebih dahulu
 # kita ambil dua data penting
 # data pertama
-df_item_masuk  = read_excel("DAFTAR ITEM TOILETRIS.xlsx",
-                            sheet = toilet_sht[2]) %>% janitor::clean_names()
+df_item_masuk  = read_excel("DAFTAR ITEM TEKNISI.xlsx",
+                            sheet = atk_sht[4]) %>% janitor::clean_names()
 # data kedua
-df_item_keluar = read_excel("DAFTAR ITEM TOILETRIS.xlsx",
-                            sheet = toilet_sht[3]) %>% janitor::clean_names()
+df_item_keluar = read_excel("DAFTAR ITEM TEKNISI.xlsx",
+                            sheet = atk_sht[3]) %>% janitor::clean_names()
 # ==============================================================================
+
 
 # ==============================================================================
 # kita akan rapikan terlebih dahulu datanya
+df_item_masuk =
+  df_item_masuk %>% 
+  filter(total_harga > 0) 
+
 df_item_keluar = 
   df_item_keluar %>% 
-  filter(!is.na(jml_keluar))
+  filter(jml_keluar > 0) 
 # ==============================================================================
 
 
@@ -44,33 +48,31 @@ df_item_keluar =
 # kita save terlebih dahulu data itemnya terlebih dahulu
 dbase_produk = 
   df_item_masuk %>% 
-  select(kode_item,nama_item,satuan,harga_beli) %>% 
+  select(kode_item,nama_item,satuan,penjual,harga_satuan) %>% 
   distinct() %>% 
-  mutate(harga_beli = as.numeric(harga_beli)) |>
-  group_by(kode_item,nama_item,satuan) |>
-  summarise(harga_beli = mean(harga_beli,na.rm = T)) |>
-  ungroup() |>
   arrange(kode_item) 
 
 # kita ambil data item masuk
 df_item_masuk = 
   df_item_masuk %>% 
   select(-input_oleh,-no_transaksi,-nama_item,
-         -harga_beli,-total_beli,-penjual,-periode,-nomor_po)
+         -harga_satuan,-total_harga,-tahun,
+         -periode) %>% 
+  rename(dibeli_dari = penjual)
 
 # kita ambil data item keluar
 df_item_keluar = 
   df_item_keluar |>
-  select(-input_oleh,-nama_item,-keterangan,
-         -no_transaksi,-periode)
+  select(-input_oleh,-nama_item,-tahun,-periode,
+         -proyek,-no_transaksi)
 
 # kita akan export kembali
-setwd("~/melek-data/Clean/Toileteries")
+setwd("~/melek-data/Clean/Listrik")
 
 openxlsx::write.xlsx(dbase_produk,
                      file = "dbase produk.xlsx")
 openxlsx::write.xlsx(df_item_masuk,
                      file = "dbase pembelian produk.xlsx")
 openxlsx::write.xlsx(df_item_keluar,
-                     file = "dbase pemberian produk.xlsx")
+                     file = "dbase pengeluaran produk.xlsx")
 # ==============================================================================
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# melek-data
-Repo untuk persiapan workshop melek data
+# _Workshop_ Melek Data
+
+_Repository_ untuk persiapan _workshop_ melek data Nutrifood 2024.
+
+## Masalah dan Data yang Dihadapi
+
+Ada tiga masalah yang akan diberikan kepada _trainee_, yakni:
+
+1. Data alat tulis kantor,
+1. Data _toileteries_, dan
+1. Data barang teknisi dan listrik.
